2c01735030c3b7f3f4e0aeab563acae489de31dc
Khah410 <168801590+Khah410@users.noreply.github.com>
2024-10-06 00:28:05 +0700

    Terraform
    Thuan commit

---
+++ Module/route tables/version.tf
@@ -0,0 +1,11 @@
+# Terraform Version
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.0"
+    }
+  }
+}
