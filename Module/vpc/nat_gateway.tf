89b43650d8dceaab6f19c696dd4e1abd8fea2597
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-10-06 11:31:39 +0700

    Thuan commit : create NAT Gateway

--- Module/vpc/nat_gateway.tf
+++ Module/vpc/nat_gateway.tf
@@ -1,2 +1,14 @@
+resource "aws_eip" "nat_eip" {
+  depends_on = [ aws_internet_gateway.i_gw ]
+}
 
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id    
+  subnet_id = aws_subnet.public_subnets.id
+  depends_on = [ aws_internet_gateway.i_gw ]
+
+  tags = {
+    Name = "Thuan_NAT_GW"
+  }
+}
 
