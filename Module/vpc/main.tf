69e2f0e98f49bc3b7cb2805f57fe4c556be8cd43
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-10-06 09:45:58 +0700

    Thuan commit create route tables public

--- Module/vpc/main.tf
+++ Module/vpc/main.tf
@@ -39,3 +39,26 @@ resource "aws_internet_gateway" "gw" {
     Name = "Thuan_IGW"
   }
 }
+
+
+# Create Route Tables Public
+resource "aws_route_table" "rt_public" {
+  vpc_id = aws_vpc.Thuan_VPC.id
+  
+  route    {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  } 
+
+
+  tags = {
+    Name = "RT_Public"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_asso" {
+  count = length(var.public_subnet_cidrs)
+  subnet_id = element(aws_subnet.public_subnets[*].id, count.index)
+  route_table_id = aws_route_table.rt_public.id
+}
+
