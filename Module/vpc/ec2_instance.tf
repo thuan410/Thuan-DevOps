b5cdf21dc7e567375fe212328aa69d230f64fbc1
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-10-06 22:14:00 +0700

    Thuan commit : create a ec2 instance with subnet (subnet private)

--- Module/vpc/ec2_instance.tf
+++ Module/vpc/ec2_instance.tf
@@ -20,12 +20,30 @@ resource "aws_instance" "public_instance" {
   key_name               = aws_key_pair.key_pair.key_name
   vpc_security_group_ids = [aws_security_group.sg_ec2.id]
   subnet_id = aws_subnet.public_subnets.id
+associate_public_ip_address = false
 
   tags = {
     Name = "public_instance"
   }
 
   root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+  }
+}
+
+
+resource "aws_instance" "private_instance" {
+   ami                    = "ami-0866a3c8686eaeeba"
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.key_pair.key_name
+   vpc_security_group_ids = [aws_security_group.sg_ec2.id]
+   subnet_id = aws_subnet.private_subnets.id
+
+   tags = {
+     Name = "private_ec2"
+   }   
+    root_block_device {
     volume_size = 30
     volume_type = "gp2"
   }
