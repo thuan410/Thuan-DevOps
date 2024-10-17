f5b826f6c51d941733d6cb3ce9c21165901d1fb6
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-10-17 19:14:02 +0700

    Thuan commit: tao user

--- project/iam/main.tf
+++ project/iam/main.tf
@@ -1,8 +1,34 @@
+# thiết lập user iam
 resource "aws_iam_user" "this" {
   count = var.create_user ? 1: 0
 
   name = var.name
 
   tags = var.tags
+}
+
+# tạo khóa để liên kết với aws
+resource "aws_iam_access_key" "this" {
+  count = var.create_user && var.create_iam_access_key ? 1 : 0
+
+  user = aws_iam_user.this[0].name
+  pgp_key = var.pgp_key
+  status = var.iam_access_key_status
+}
+
+resource "aws_iam_access_key" "this_no_pgp" {
+  count = var.create_user && var.create_iam_access_key && (var.pgp_key != null && var.pgp_key != "")  ? 1 : 0
+
+  user = aws_iam_user.this[0].name
+  pgp_key = var.pgp_key
+  status = var.iam_access_key_status
+}
+
+# Them chinh sach cho cac user
+resource "aws_iam_user_policy_attachment" "this" {
+  for_each = { for k, v in var.policy_arns : k => v if var.create_user }
+
+  user       = aws_iam_user.this[0].name
+  policy_arn = each.value
 }
 
--- project/iam/variables.tf
+++ project/iam/variables.tf
@@ -15,3 +15,35 @@ variable "tags" {
   type = map(string)
   default = { }
 }
+
+# tạo key để có thể kết nối với AWS
+variable "create_iam_access_key" {
+  description = "Bạn có muốn tạo khóa IAM không?"
+  type = bool
+  default = true
+}
+
+variable "pgp_key" {
+  description = "value"
+  type = string
+  default = ""
+}
+
+# Kiểm tra trạng thái
+variable "iam_access_key_status" {
+  description = "Access key status to apply"
+  type = string
+  default = null
+}
+
+# thêm quyền cho các user 
+variable "policy_arns" {
+  description = "danh sach ARNS cua chinh sach den voi IAM user"
+  type = list(string)
+  default = [
+    "arn:aws:iam::aws:policy/AdministratorAccess",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+  ]
+  
+}
+
