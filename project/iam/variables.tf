5834477f5c0c91db3133f6695c56ac1df54878fb
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-11-05 17:50:01 +0700

    Thuan commit: create key

--- project/iam/variables.tf
+++ project/iam/variables.tf
@@ -3,3 +3,15 @@ variable "create_user" {
   type = string
   default = "Test123"
 }
+
+variable "create_iam_access_key" {
+  description = "IAM access key"
+  type = string
+  default = "test"
+}
+
+variable "pgp_key" {
+  description = "Either a base-64 encoded PGP public key, or a keybase username in the form `keybase:username`. Used to encrypt password and access key."
+  type        = string
+  default     = ""
+}
