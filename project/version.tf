f6b3631cf83578fc07123931b104753bdfe7d420
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-11-05 17:05:22 +0700

    Thuan commit

---
+++ project/version.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+terraform {
+  required_version = " >= 1.0.0"
+}
---
+++ project/vpc/output.tf
@@ -0,0 +1,4 @@
+output "vpc_id" {
+  description = "ID of project VPC"  
+  value = var.vpc_cidr_block
+}
