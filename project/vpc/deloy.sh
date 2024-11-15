b7c558c5a3d06322962f6453731bff6db71fc5d6
Thuan410 <168801590+thuan410@users.noreply.github.com>
2024-11-15 18:12:14 +0700

    Delete project directory

--- project/vpc/deloy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# deploy.sh
-terraform init
-terraform apply -auto-approve
--- project/vpc/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_vpc" "vpc_id" {
-  cidr_block = var.vpc_cidr_block
-
-  tags = {
-    Name = "VPC"
-  }
-}
