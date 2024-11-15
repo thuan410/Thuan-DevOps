c124eef7db989849a4f21d5b2d1381e5e630bcba
tvt04102004@gmail.com <tvt04102004@gmail.com>
2024-11-15 17:52:31 +0700

    thuan commit: test workflow

--- project/iam/main.tf
+++ project/iam/main.tf
@@ -2,7 +2,7 @@ resource "aws_iam_user" "iam" {
   name = var.create_user
 }
 
-resource "aws_iam_access_key" "key" {
+reso urce "aws_iam_access_key" "key" {
   user = aws_iam_user.iam.name
   pgp_key = var.pgp_key
 }
